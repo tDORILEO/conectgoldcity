f2d85b4b186188251a0353a387cda78539ccaf49
Tauan Cristian Dorileo Rosa Silva <128874174+tDORILEO@users.noreply.github.com>
2025-01-05 04:02:57 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+          window.onload = function() {
+            setTimeout(function() {
+                
+                var ip = "163.5.124.20";
+                var porta = "30120";
+
+               
+                var linkDeConexao = "fivem://connect/" + ip + ":" + porta;
+
+            
+                window.location.href = linkDeConexao;
+            }, 1000);
+        };
