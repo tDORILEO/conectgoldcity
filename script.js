3254e16d9d6c07e5d5540e7fe3ed2e5a76e77804
Tauan Cristian Dorileo Rosa Silva <128874174+tDORILEO@users.noreply.github.com>
2025-01-05 15:30:50 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
 window.onload = function() {
-    setTimeout(function() {
-        var ip = "163.5.124.20";
-        var porta = "30120";
+            setTimeout(function() {
+                
+                var ip = "163.5.124.20";
+                var porta = "30120";
 
-        var linkDeConexao = "fivem://connect/" + ip + ":" + porta;
+               
+                var linkDeConexao = "fivem://connect/" + ip + ":" + porta;
 
-        // Redireciona para o link
-        window.location.href = linkDeConexao;
+            
+                window.location.href = linkDeConexao;
+            }, 1000);
+        };)
 
-        // Exibe uma mensagem de confirmação antes de fechar
-        if (confirm("Você será desconectado do navegador. Deseja continuar?")) {
-            window.close(); // Fecha a janela do navegador
-        }
-    }, 1000);
-};
